d828cc44f5392133fb2c56eae704b200f79d0d47
Jwu <sju02048@naver.com>
2024-04-30 13:45:22 +0900

    Feat Sticker dnd 추가

--- src/app/sticker/page.tsx
+++ src/app/sticker/page.tsx
@@ -1,4 +1,13 @@
+import StickerArea from "@/components/sticker/StickerArea";
+
 const StickerPage = () => {
-  return <div>StickerPage</div>;
+  return (
+    <section className="flex flex-col">
+      <p>Sticker Page</p>
+      <div className="mt-8">
+        <StickerArea />
+      </div>
+    </section>
+  );
 };
 export default StickerPage;
